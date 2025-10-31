15c15dbd69a7356fe1203c8b43e493972a16c325
MohamadMilly <volassoa@gmail.com>
2025-11-01 01:32:18 +0300

    Update SSL configuration to allow unauthorized connections

--- db/pool.js
+++ db/pool.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    ca: fs.readFileSync("./ca.pem").toString(),
-    rejectUnauthorized: true
+    rejectUnauthorized:false,
   },
 });
