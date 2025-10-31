0eb34ba973143c2cbc08d371eaca7cfca5f02c09
MohamadMilly <volassoa@gmail.com>
2025-11-01 01:25:59 +0300

    Fix SSL certificate path in pool.js

--- db/pool.js
+++ db/pool.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    ca: fs.readFileSync("../ca.pem").toString(),
+    ca: fs.readFileSync("./ca.pem").toString(),
     rejectUnauthorized: true
   },
 });
