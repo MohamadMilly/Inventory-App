57af9d8154b627d0449db04d94e63f06ba36a20e
MohamadMilly <volassoa@gmail.com>
2025-11-01 09:19:58 +0300

    using ca.pem from aiven

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,12 @@
 const { Pool } = require("pg");
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false, require: true },
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(path.join(__dirname, "../ca.pem")).toString(),
+  },
 });
